85147e86a16bf5417614f9f4decabc97875e3a3e
Iman Maliki <iman@dne.co.id>
2023-06-15 11:50:01 +0700

    final web

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,9 +1,11 @@
 $('#navigation a').on('click', function (e) {
-    e.preventDefault();
-    var hash = this.hash;
-    $('html, body').animate({
-        scrollTop: $(this.hash).offset().top
-    }, 1000);
+    if((this.href).includes('#')) {
+        e.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    };
 });
 
 
